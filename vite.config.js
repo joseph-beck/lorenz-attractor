eaf09285bf01ee4f865ad0d7b11f3cc82297ecd5
= <=>
2023-02-01 14:55:49 +0000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import * as THREE from 'three';
-import gsap from 'gsap'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/lorenz/',
-    plugins: [THREE, gsap]
+    plugins: []
 });
